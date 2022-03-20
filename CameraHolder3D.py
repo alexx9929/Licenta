a939194087927352a2bc8390d5746ea727336bd8
Alex Şerban <alexserban2207@gmail.com>
2022-03-21 00:27:29 +0200

    added cameraHolder

--- CameraHolder3D.py
+++ CameraHolder3D.py
@@ -4,10 +4,8 @@ import DIContainer
 
 
 class Camera3D(Qt3DRender.QCamera):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, viewCamera):
+        super().__init__(viewCamera)
         self.lens().setPerspectiveProjection(45, 16 / 9, 0.1, 1000)
         self.setPosition(QVector3D(0, 0, 40))
         self.setViewCenter(QVector3D(0, 0, 0))
-
-        DIContainer.view.camera = self
