ae806c6ffab9aa16b43635b6377c9b091c24c39a
Alex Şerban <alexserban2207@gmail.com>
2022-05-25 00:46:36 +0300

    changed how images are read + added histogram

--- ObjectBuilding/Visuals/TextureImage.py
+++ ObjectBuilding/Visuals/TextureImage.py
@@ -2,8 +2,10 @@ from PySide6.QtGui import QImage, QPainter
 from PySide6.QtCore import QRect, QSize, Qt
 from PySide6.Qt3DRender import Qt3DRender
 import os
-
+import numpy as np
+import cv2
 import DIContainer
+from matplotlib import pyplot as plt
 
 
 class TextureImage(Qt3DRender.QPaintedTextureImage):
@@ -16,12 +18,29 @@ class TextureImage(Qt3DRender.QPaintedTextureImage):
         # Setting sizes
         self.setWidth(width)
         self.setHeight(height)
+
+        self.histogram = None
         pass
 
     def paint(self, painter: QPainter):
         if not self.filename:
             return
 
-        painter.drawImage(QRect(0, 0, self.width(), self.height()),
-                          QImage(os.path.join(DIContainer.main_window.defaultImageDirectory, self.filename))
-                          .scaled(QSize(self.width(), self.height()), Qt.IgnoreAspectRatio, Qt.SmoothTransformation))
+        path = os.path.join(DIContainer.main_window.defaultImageDirectory, self.filename)
+        cv_img = cv2.imread(path)
+        height, width, channel = cv_img.shape
+        bytes_per_line = 3 * width
+        image = QImage(cv_img.data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped().scaled(
+            QSize(self.width(), self.height()), Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
+
+        # define colors to plot the histograms
+        colors = ('b', 'g', 'r')
+
+        # compute and plot the image histograms
+        for i, color in enumerate(colors):
+            hist = cv2.calcHist([cv_img], [i], None, [256], [0, 256])
+            plt.plot(hist, color=color)
+        plt.title('Histogram of ' + self.filename)
+        plt.show()
+
+        painter.drawImage(QRect(0, 0, self.width(), self.height()), image)
